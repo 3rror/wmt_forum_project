bb79e1bcae9b0f4fc12fb438970291e8bf7f9bb6
Gianluca Andreotti <gianlucaatlas@gmail.com>
2020-06-22 14:26:36 +0200

    Reformat application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -16,20 +15,19 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-
 // Action-text
-require("trix")
-require("@rails/actiontext")
-
+require("trix");
+require("@rails/actiontext");
 
 import "normalize.css";
-import './application.scss'
+import "./application.scss";
 import "@fortawesome/fontawesome-free/css/all.css";
 
-
 function expandSearchBar(event) {
-    let el = document.getElementById("query").addEventListener('click', function(e) {
-        document.getElementById("site-nav").classList.add("searching");
+  let el = document
+    .getElementById("query")
+    .addEventListener("click", function (e) {
+      document.getElementById("site-nav").classList.add("searching");
     });
 }
 document.addEventListener("turbolinks:load", expandSearchBar);
